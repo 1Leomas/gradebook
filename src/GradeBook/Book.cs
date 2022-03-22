cd3126ef0db93074b6bb7ffd997387a54992e2e9
Samuil Rutcovschi <samuil-pavel.rutcovschi@endava.com>
2022-03-22 23:11:45 +0200

    Add method ShowStatistics()

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -14,6 +15,26 @@ namespace GradeBook
         public void AddGrade(double grade) //method
         {
             grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach(var number in grades) 
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            //Count contains numeber of items in the list
+            result = result / grades.Count; 
+
+            //:N2 - limits float number to only 2 digits
+            Console.WriteLine($"The average grade is {result:N2}");
+            Console.WriteLine($"The highest grade is {highGrade:N2}");
+            Console.WriteLine($"The lowest grade is {lowGrade:N2}");
         }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -12,24 +12,7 @@ namespace GradeBook
             book.AddGrade(12.4); 
             book.AddGrade(77.5); 
 
-            var grades = new List<double>() { 3.2, 3.4, 3.3, 5.7}; 
-            
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowestGrade = double.MaxValue;
-            foreach(var number in grades) 
-            {
-                highGrade = Math.Max(number, highGrade);
-                lowestGrade = Math.Min(number, lowestGrade);
-                result += number;
-            }
-            //Count contains numeber of items in the list
-            result = result / grades.Count; 
-
-            //:N2 - limits float number to only 2 digits
-            Console.WriteLine($"The average grade is {result:N2}");
-            Console.WriteLine($"The highest grade is {highGrade:N2}");
-            Console.WriteLine($"The highest grade is {lowestGrade:N2}");
+            book.ShowStatistics();
         }
     }
 }
