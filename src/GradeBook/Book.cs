c2e181a3090716f4eda943f0de89a91ce37a5298
Samuil Rutcovschi <samuil-pavel.rutcovschi@endava.com>
2022-03-22 22:33:59 +0200

    Compute highest and lowest grade

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -4,13 +4,14 @@ namespace GradeBook
 {
     class Book
     {
-        List<double> grades; //fild
-
-        public Book()
+        private List<double> grades; //fild
+        private string name;
+        public Book(string name) //constructor
         {
+            this.name = name;
             grades = new List<double>();
         }
-        public void AddGrade(double grade)
+        public void AddGrade(double grade) //method
         {
             grades.Add(grade);
         }
