dbf70a04dabefdb181a37b1d5b6c85a064889dc5
Samuil Rutcovschi <samuil-pavel.rutcovschi@endava.com>
2022-03-22 16:59:30 +0200

    learn array syntax

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,10 +6,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            if(args.Length > 0)
-                Console.WriteLine($"Hello, {args[0]}!");
-            else
-                Console.WriteLine($"Hello!");
+            //Initialization types
+            double[] numbers = new double[3] { 1.2, 3.4, 4.1};
+            double[] numbers_1 = new double[] { 1.2, 3.4, 4.1};
+            double[] numbers_2 = new [] { 1.2, 3.4, 4.1};
+            double[] numbers_3 = { 1.2, 3.4, 4.1};
+            
+            var result = numbers[0];
+            result += numbers[1];
+            Console.WriteLine(numbers[2]);
+            //Console.WriteLine($"Result is: {result}");
         }
     }
 }
