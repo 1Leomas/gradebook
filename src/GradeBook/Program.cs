4855f5407068432e410ba01fec4ccb373a0e151b
Samuil Rutcovschi <samuil-pavel.rutcovschi@endava.com>
2022-03-22 17:12:08 +0200

    use array to sum an array

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,16 +6,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            //Initialization types
-            double[] numbers = new double[3] { 1.2, 3.4, 4.1};
-            double[] numbers_1 = new double[] { 1.2, 3.4, 4.1};
-            double[] numbers_2 = new [] { 1.2, 3.4, 4.1};
-            double[] numbers_3 = { 1.2, 3.4, 4.1};
+            double[] numbers = new double[3] { 3.2, 3.4, 3.3};
+
+            var result = 0.0;
             
-            var result = numbers[0];
-            result += numbers[1];
-            Console.WriteLine(numbers[2]);
-            //Console.WriteLine($"Result is: {result}");
+            foreach(var number in numbers)
+            {
+                result += number;
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
